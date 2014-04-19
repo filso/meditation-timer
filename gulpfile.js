897ea0250fcd9010d630c678cd35783a0c13bd03
Filip Sobczak <filsob@gmail.com>
2014-04-19 22:17:35 +0200

    addadd dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  sass = require('gulp-sass'),
+  minifyCss = require('gulp-minify-css'),
+  rename = require('gulp-rename'),
+  karma = require('gulp-karma'),
+  ngmin = require('gulp-ngmin'),
+  plumber = require('gulp-plumber'),
+  stylish = require('jshint-stylish'),
+  jshint = require('gulp-jshint'),
+  ngHtml2Js = require('gulp-ng-html2js'),
+  rev = require('gulp-rev'),
+  size = require('gulp-size'),
+  jade = require('gulp-jade'),
+  linker = require('gulp-linker'),
+  runSequence = require('run-sequence'),
+  livereload = require('gulp-livereload');
+
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -15,7 +28,9 @@ gulp.task('sass', function(done) {
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
